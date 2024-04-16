4186dd419dbbb14100aab07d160e64e9a55b5a71
José Cortés molina <josecortes@Joses-MacBook-Pro.local>
2024-04-16 10:42:52 -0400

    update code to labda file and config aws credentials

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-const spreadsheet = require('./src/spreadsheet');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { JWT } = require('google-auth-library');
+require('dotenv').config();
 
-spreadsheet.accessToGoogleSuite();
+const DOCUMENT_ID = '1Rn2HBMAlw4C3gGW-5PSn1xdUAr1gYqeC61sFWyuvtqw';
+
+const serviceAccount = new JWT({
+  email: process.env.CLIENT_EMAIL,
+  key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+async function accessToGoogleSuite() {
+  try {
+    const document = new GoogleSpreadsheet(DOCUMENT_ID, serviceAccount);
+    await document.loadInfo();
+
+    const sheet = document.sheetsByIndex[0];
+
+    //const registers = await sheet.getRows();
+    sheet.addRows([
+      {
+        data: 'John Doe',
+      },
+    ]);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const handler = async (event) => {
+  await accessToGoogleSuite();
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Google Sheet updated!'),
+  };
+  return response;
+};
